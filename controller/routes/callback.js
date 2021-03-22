280479a8b3baf75f8f45100f0916b5424da9e204
benl95 <beno_1995@hotmail.com>
2021-03-22 16:43:16 +0100

    feat: modulize routing

---
+++ controller/routes/callback.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const spotifyWebApi = require('spotify-web-api-node');
+const { reqTokens } = require('../../public/js/helpers/reqTokens');
+
+const api = new spotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	redirectUri: process.env.REDIRECT_URI,
+});
+
+router.get('/', (req, res) => {
+	reqTokens(req.query.code)
+		.then((tokens) => {
+			api.setAccessToken(tokens.access_token);
+			api.setRefreshToken(tokens.refresh_token);
+
+			req.session.access_token = tokens.access_token;
+			req.session.refresh_token = tokens.refresh_token;
+			req.session.save();
+		})
+		.then(() => {
+			res.redirect('/playlists');
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+module.exports = router;
---
+++ controller/routes/playlists.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { transformData } = require('../../public/js/data/transformData');
+const { fetchId } = require('../../public/js/helpers/fetchID');
+const { fetchPlaylists } = require('../../public/js/helpers/fetchPlaylists');
+const spotifyWebApi = require('spotify-web-api-node');
+
+const api = new spotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	redirectUri: process.env.REDIRECT_URI,
+});
+
+router.get('/', (req, res) => {
+	const tokens = {
+		access_token: req.session.access_token,
+		refresh_token: req.session.refresh_token,
+	};
+
+	api.setAccessToken(tokens.access_token);
+	api.setRefreshToken(tokens.refresh_token);
+
+	fetchId(tokens)
+		.then((userId) => {
+			let playlists = fetchPlaylists(tokens, userId);
+			return playlists;
+		})
+		.then((playlists) => {
+			let transformPlaylists = transformData(playlists);
+
+			res.render('playlists', {
+				title: 'playlists',
+				data: transformPlaylists,
+				access_token: tokens.access_token,
+				refresh_token: tokens.refresh_token,
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+module.exports = router;
