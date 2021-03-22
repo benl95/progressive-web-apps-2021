bb9f5f408064e6eb9859acb7253e71150faf374f
benl95 <beno_1995@hotmail.com>
2021-03-22 16:42:57 +0100

    feat: modulize token fetching

---
+++ public/js/helpers/reqTokens.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const spotifyWebApi = require('spotify-web-api-node');
+
+const api = new spotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	redirectUri: process.env.REDIRECT_URI,
+});
+
+async function reqTokens(code) {
+	let response = api.authorizationCodeGrant(code);
+	let data = await response;
+
+	const tokens = {
+		access_token: data.body['access_token'],
+		refresh_token: data.body['refresh_token'],
+	};
+
+	return Promise.resolve(tokens);
+}
+
+module.exports = { reqTokens };
