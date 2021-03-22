f94ce8acedcf064984ad389cca2bf60213feca09
benl95 <beno_1995@hotmail.com>
2021-03-22 16:54:56 +0100

    feat: import routing modules, remove waterfall code

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const spotifyWebApi = require('spotify-web-api-node');
 const hbs = require('express-handlebars');
-const { authSpotifyApi } = require('./public/js/helpers/spotifyAuth.js');
-const { transformData } = require('./public/js/data/transformData');
+const session = require('express-session');
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
 
 const login = require('./controller/routes/login');
 const auth = require('./controller/routes/auth');
-
-const api = new spotifyWebApi({
-	clientId: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET,
-	redirectUri: process.env.REDIRECT_URI,
-});
+const callback = require('./controller/routes/callback');
+const playlists = require('./controller/routes/playlists');
+const tracks = require('./controller/routes/tracks');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/controller/views');
@@ -28,63 +23,21 @@ app.engine(
 );
 
 app.use(express.static(path.join(__dirname, '/public')))
-	.use('/', login)
-	.use('/login', auth);
-
-app.get('/callback', (req, res) => {
-	authSpotifyApi(req.query.code)
-		.then((tokens) => {
-			api.setAccessToken(tokens.access_token);
-			api.setRefreshToken(tokens.refresh_token);
+	.use(
+		session({
+			secret: 'secret-key',
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				secure: 'auto',
+			},
 		})
-		.then(() => {
-			res.redirect('/playlists');
-		})
-		.catch((error) => {
-			res.status(error.code);
-			res.send(error.message);
-		});
-});
-
-// Fetch playlists of user
-app.get('/playlists', (req, res) => {
-	api.getMe()
-		.then((data) => {
-			const id = data.body.id;
-			return id;
-		})
-		.then((id) => {
-			const userPlaylists = api.getUserPlaylists(id);
-			return userPlaylists;
-		})
-		.then((userPlaylists) => {
-			const playlistItems = transformData(userPlaylists.body);
-
-			res.render('playlists', {
-				title: 'Your playlists',
-				data: playlistItems,
-			});
-		})
-		.catch((error) => {
-			console.error(error);
-		});
-});
-
-// Fetch playlist tracks based on @params id in url
-app.get('/tracks/:id', (req, res) => {
-	api.getPlaylistTracks(req.params.id)
-		.then((data) => {
-			const trackItems = transformData(data.body);
-
-			res.render('tracks', {
-				title: 'Playlist tracks',
-				data: trackItems,
-			});
-		})
-		.catch((error) => {
-			console.error(error);
-		});
-});
+	)
+	.use(login)
+	.use(auth)
+	.use(callback)
+	.use(playlists)
+	.use(tracks);
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
