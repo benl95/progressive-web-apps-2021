0556c1d673a9c34e49c6cb548d20f6c5a8a920c6
benl95 <beno_1995@hotmail.com>
2021-03-27 10:52:30 +0100

    docs: remove parseData module, no more use

--- helpers/utils/parseData.js
+++
@@ -1,9 +0,0 @@
-function parseData(data) {
-	const stringify = JSON.stringify(data);
-	const parsed = JSON.parse(stringify);
-	const items = parsed.body.items;
-
-	return items;
-}
-
-module.exports = { parseData };
